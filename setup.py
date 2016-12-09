981b3de27363e8248034316e6f860850794f212a
Ashwin Kapur <akapur@amvirk.com>
2016-12-09 15:05:29 -0500

    Formatting changes.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+# coding=utf-8
+"""Setup pycomputrac"""
+
 from setuptools import setup
 
 setup(name='pycomputrac',
       version='0.1',
-      description='Read Metastock/Computrac data stored in files with emaster/master files',
+      description='Read market data stored in Computrac/Metastock format',
       url='https://github.com/akapur/pycomputrac',
       author='Ashwin Kapur',
       author_email='akapur@amvirk.com',
